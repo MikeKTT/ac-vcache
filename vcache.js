854a4be759ac8c8db2b94579cf064cd94cd29b71
Michael Morris <mike@moresteam.co.uk>
2015-01-20 18:31:31 +0000

    Template and controller changes
    Added a fixed header with a placeholder dropdown for recently searched
    queries.
    
    Replaced image placeholders with ng-repeat snippet.

--- vcache.js
+++ vcache.js
@@ -1,13 +1,17 @@
 angular.module('vCacheBrowser', [])
   .controller('vCacheBrowserController', ['$scope', function($scope) {
   
-  	// Define our defaults 
-  	$scope.stockRefInput = "ARNFC-U-7276";
+  	// Define default input values
+  	$scope.stockRefInput = "seems-irrelevant";
   	$scope.registrationInput = "YT61BXJ";
   	
+
   	// Encode a stock reference & registration and set stockIdentifier
     $scope.encodeInput = function() {
 		// Reset and prepare array / variables
+		
+		$scope.registrationInput = $scope.registrationInput.replace(/\s+/g, '').toUpperCase();
+		
 		var stockRef = $scope.stockRefInput.split("");
 		var regReverse = $scope.registrationInput.split("").reverse();
 		var pointer = 0;
@@ -21,16 +25,18 @@ angular.module('vCacheBrowser', [])
 
 		// Append the 9th character
 		$scope.stockIdentifier = ($scope.stockIdentifier+stockRef[8]).toUpperCase();
+		
     };
  
+	$scope.imageSources = { variants: ['f', 'r', 'i', '4', '5', '6'] };
+     
 	$scope.fetchResults = function() {     
-		 $scope.encodeInput(); 
+		 $scope.encodeInput();  
 		 console.log($scope.stockIdentifier);
       
     };
     
     $scope.fetchResults();
- 
   }]);
   
   
