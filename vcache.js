8b0ce045305924c8ac3b8c09071b4a898adca3f7
Michael Morris <mike@moresteam.co.uk>
2015-01-21 18:08:08 +0000

    Finishing touches
    Removed the dropdown concept.
    
    Bound the modal links to an Angular function to set a currentImage
    variable for the full size image source and title.
    
    Removed unnecessary CSS and added viewport META for responsive view.

--- vcache.js
+++ vcache.js
@@ -1,42 +1,37 @@
 angular.module('vCacheBrowser', [])
-  .controller('vCacheBrowserController', ['$scope', function($scope) {
+  .controller('vCacheBrowserCtrl', ['$scope', function($scope) {
   
-  	// Define default input values
-  	$scope.stockRefInput = "seems-irrelevant";
+  	// Define default values and data
+  	$scope.stockRefInput = "ARNFC-U-7276";
   	$scope.registrationInput = "YT61BXJ";
-  	
+  	$scope.currentImage = "f";
+	$scope.imageSources = { variants: ['f', 'r', 'i', '4', '5', '6'] };
 
   	// Encode a stock reference & registration and set stockIdentifier
     $scope.encodeInput = function() {
 		// Reset and prepare array / variables
-		
 		$scope.registrationInput = $scope.registrationInput.replace(/\s+/g, '').toUpperCase();
 		
-		var stockRef = $scope.stockRefInput.split("");
+		var stockRef = ($scope.stockRefInput.length < 10 ? "XXXXX-XXXX" : $scope.stockRefInput).split("");
 		var regReverse = $scope.registrationInput.split("").reverse();
 		var pointer = 0;
 		$scope.stockIdentifier = "";
 
 		// Loop through the reversed registration and place characters from stockRef
 		angular.forEach(regReverse, function(value) {
-		$scope.stockIdentifier = ($scope.stockIdentifier+stockRef[pointer]+value);
-		pointer++;
+			$scope.stockIdentifier = ($scope.stockIdentifier+stockRef[pointer]+value);
+			pointer++;
 		}, regReverse);
 
 		// Append the 9th character
 		$scope.stockIdentifier = ($scope.stockIdentifier+stockRef[8]).toUpperCase();
-		
-    };
- 
-	$scope.imageSources = { variants: ['f', 'r', 'i', '4', '5', '6'] };
-     
-	$scope.fetchResults = function() {     
-		 $scope.encodeInput();  
-		 console.log($scope.stockIdentifier);
-      
     };
     
-    $scope.fetchResults();
-  }]);
+    $scope.bigPicture = function(variantID) {
+		$scope.currentImage = variantID;
+    };
+    
+    $scope.encodeInput();
+}]);
   
   
